e785477cd15a04a6eab3c86c719b92d00ae1f7f0
walekaratul <walekaratul90@gmail.com>
2026-01-18 13:41:13 +0530

    conflict reolve

--- atul/src/main/java/atul/test.java
+++ atul/src/main/java/atul/test.java
@@ -5,11 +5,11 @@ public class test {
 	
 	public static void main(String[] args) {
 		System.out.println("Hii---a");
-<<<<<<< Updated upstream
+
 		System.out.println("Hii---b");
-=======
+
 		System.out.println("Hii---cada");
->>>>>>> Stashed changes
+ 
 		
 	}
 
